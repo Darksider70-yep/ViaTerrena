d2900647ba6b5a4fb52c2bc2f18f7c8e8e573707
Darksider70-yep <darksider70117@gmail.com>
2026-04-26 23:54:33 +0530

    feat: configure Google Maps API key in Android app manifest and config

--- app.config.js
+++ app.config.js
@@ -3,9 +3,18 @@ require('dotenv').config();
 module.exports = ({ config }) => {
   return {
     ...config,
+    android: {
+      ...config.android,
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_PLACES_API_KEY || "",
+        },
+      },
+    },
     extra: {
       ...config.extra,
       googlePlacesApiKey: process.env.GOOGLE_PLACES_API_KEY || "",
     },
   };
 };
+
